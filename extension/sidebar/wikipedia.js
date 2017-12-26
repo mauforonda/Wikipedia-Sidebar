4e9dd16242fd549fd4238145e82e790772a48c9c
Mauricio Foronda <josemauricioforonda@gmail.com>
2017-12-26 11:26:43 -0400

    handle cases with no exact page by making full text searches

--- extension/sidebar/wikipedia.js
+++ extension/sidebar/wikipedia.js
@@ -1,3 +1,155 @@
+function arrowNavigation() {
+  wLinks = document.getElementsByClassName('wiki')
+  selected = 0
+  wLinks[selected].classList.add('selected')
+  var map = {}      
+  function navigate(e) {
+    e.preventDefault()
+    map[e.keyCode] = e.type == 'keydown'
+    
+    // Enter
+    if(map[13]){
+      wLinks[selected].click()
+      body.removeEventListener('keydown', navigate)
+      body.removeEventListener('keyup', navigate)
+      
+      // Up          
+    } else if(map[38]) {
+      if (selected > 0) {
+        wLinks[selected].classList.remove('selected')
+        selected--
+        wLinks[selected].classList.add('selected')
+        wLinks[selected].scrollIntoView({behavior: "instant", block: "nearest"})
+      }
+      
+      // Down
+    } else if (map[40]) {
+      if (selected < wLinks.length - 1) {
+        wLinks[selected].classList.remove('selected')
+        selected++
+        wLinks[selected].classList.add('selected')
+        wLinks[selected].scrollIntoView({behavior: "instant", block: "nearest"})
+      }
+      
+      // Ctrl+Shift+V
+    } else if (map[17] && map[16] && map[86]) {
+      browser.sidebarAction.close()
+
+      // Home
+    } else if (map[36]) {
+      window.scroll(0,0)
+
+      // End
+    } else if (map[35]) {
+      window.scroll(0,document.body.scrollHeight)
+    }
+  }
+  body.addEventListener('keydown', navigate)
+  body.addEventListener('keyup', navigate)
+}
+
+// Full search
+function fullSearch(selection, pageOffset) {
+  body = document.body
+  url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=1&indexpageids=&list=search&srlimit=20&srprop=snippet&sroffset='+pageOffset+'&srsearch='+selection
+  urlString = fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      hits = data.query.searchinfo.totalhits
+
+      // No search result
+      if (hits == 0) {
+        
+        // Follow suggested search terms
+        if (data.query.searchinfo.suggestion) {
+          fullSearch(data.query.searchinfo.suggestion, 0)
+          
+          // No suggestions
+        } else {
+        noArticle = document.createElement('div')
+        noArticle.setAttribute('id', 'noarticle')
+        noArticle.textContent = "Couldn't find a page or search results for that"
+          body.appendChild(noArticle)
+        }
+      } else {
+        
+        // Search header
+        searchHeader = document.createElement('div')
+        searchHeader.setAttribute('id', 'searchheader')
+        if (pageOffset == 0) {
+          searchHeader1 = document.createTextNode("There's no exact page for that, but I found ")
+          searchHeaderHits = document.createElement('b')
+          searchHeaderHits.textContent = String(hits)
+          searchHeader2 = document.createTextNode(' search results on Wikipedia.')
+          searchHeaderp1 = document.createElement('p')
+          searchHeaderp1.setAttribute('id', 'searchexplain')
+          searchHeaderp1.appendChild(searchHeader1)
+          searchHeaderp1.appendChild(searchHeaderHits)
+          searchHeaderp1.appendChild(searchHeader2)
+          searchHeader.appendChild(searchHeaderp1)
+        }
+        searchHeaderp2 = document.createElement('p')
+        searchHeaderp2.setAttribute('id', 'searchcount')
+        if (hits  < pageOffset + 20 ) {
+          searchHeaderp2.textContent = "Results " + String(pageOffset) + " to " +  String(hits) + ":"
+        } else {
+          searchHeaderp2.textContent = "Results " + String(pageOffset) + " to " + String(pageOffset + 20) + ":"
+        }
+        searchHeader.appendChild(searchHeaderp2)
+        
+        // Search results
+        searchResults = document.createElement('div')
+        searchResults.setAttribute('id', 'searchresults')
+        for (i = 0 ; i < data.query.search.length ; i++){
+          result = document.createElement('div')
+          result.setAttribute('class', 'result')
+          rLink = document.createElement('a')
+          rLink.setAttribute('class', 'wiki')
+          rLink.textContent = data['query']['search'][i]['title']
+          rLink.addEventListener("click", function( event ) {
+            searchHeader.remove()
+            searchResults.remove()
+            nextPage.remove()
+            wikipediaBody(event.target.textContent)
+          })
+          rSnippet = document.createElement('p')
+          snippetText = data['query']['search'][i]['snippet'].split(RegExp('(?:<span class="searchmatch">|</span>)')).join('')
+          rSnippet.textContent = snippetText
+          hr =  document.createElement('hr')
+          result.appendChild(rLink)
+          result.appendChild(rSnippet)
+          searchResults.appendChild(hr)
+          searchResults.appendChild(result)
+        }
+
+        // Display header and results
+        body.appendChild(searchHeader)
+        body.appendChild(searchResults)
+
+        // Next page
+        pageOffset = pageOffset + 20
+        if (pageOffset < hits) {
+          nextPage = document.createElement('div')
+          nextPageLink = document.createElement('a')
+          nextPageLink.setAttribute('class', 'wiki')
+          nextPageLink.textContent = "Next 20 results"
+          nextPageLink.addEventListener('click', function(event) {
+            searchHeader.remove()
+            searchResults.remove()
+            nextPage.remove()
+            fullSearch(selection, pageOffset)
+          })
+          nextPage.appendChild(nextPageLink)
+          body.appendChild(nextPage)
+        }
+
+        // Arrow navigation
+        arrowNavigation()
+      }
+      document.getElementById('focused').remove()
+    })
+}
+
 // Disambiguation
 function disambiguate(selection) {
   
@@ -72,8 +224,8 @@ function disambiguate(selection) {
                   cLink.setAttribute('class', 'wiki')
                   cLink.textContent = c.getAttribute('title')
                   cLink.addEventListener("click", function( event ) {
-                    body.removeEventListener('keydown', navigate)
-                    body.removeEventListener('keyup', navigate)
+                    // body.removeEventListener('keydown', navigate)
+                    // body.removeEventListener('keyup', navigate)
                     suggestions.remove()
                     wikipediaBody(event.target.textContent)
                   })
@@ -94,8 +246,8 @@ function disambiguate(selection) {
                     iLink.setAttribute('class', 'wiki')
                     iLink.textContent = i.getAttribute('title')
                     iLink.addEventListener("click", function( event ) {
-                      body.removeEventListener('keydown', navigate)
-                      body.removeEventListener('keyup', navigate)
+                      // body.removeEventListener('keydown', navigate)
+                      // body.removeEventListener('keyup', navigate)
                       suggestions.remove()
                       wikipediaBody(event.target.textContent)
                     })
@@ -118,42 +270,8 @@ function disambiguate(selection) {
       // Display
       body.appendChild(suggestions)
 
-      // Navigate Wikipedia links through arrow keys
-      wLinks = document.getElementsByClassName('wiki')
-      selected = 0
-      wLinks[selected].classList.add('selected')
-      var map = {}      
-      function navigate(e) {
-        e.preventDefault()
-        map[e.keyCode] = e.type == 'keydown'
-        // Enter
-        if(map[13]){
-          wLinks[selected].click()
-          // Up          
-        } else if(map[38]) {
-          if (selected > 0) {
-            wLinks[selected].classList.remove('selected')
-            selected--
-            wLinks[selected].classList.add('selected')
-            wLinks[selected].scrollIntoView({behavior: "instant", block: "nearest"})
-          }
-          // Down
-        } else if (map[40]) {
-          if (selected < wLinks.length - 1) {
-            wLinks[selected].classList.remove('selected')
-            selected++
-            wLinks[selected].classList.add('selected')
-            wLinks[selected].scrollIntoView({behavior: "instant", block: "nearest"})
-          }
-          // Ctrl+Shift+V
-        } else if (map[17] && map[16] && map[86]) {
-          browser.sidebarAction.close()
-        }
-      }
-      body.addEventListener('keydown', navigate)
-      body.addEventListener('keyup', navigate)
-      
-      // Ugly hack to focus sidebar
+      // Arrow navigation
+      arrowNavigation()
       document.getElementById('focused').remove()
       
     })}
@@ -189,22 +307,23 @@ function wikipediaImage(selection){
 
 // Get article extract
 function wikipediaBody(selection){
-
-  url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=1&indexpageids=&prop=extracts|templates&exintro=&explaintext=&tltemplates=Template:Disambiguation|Template:Dmbox|Template:Biology_disambiguation|Template:Call_sign_disambiguation|Template:Caselaw_disambiguation|Template:Chinese_title_disambiguation|Template:Disambiguation_cleanup|Template:Genus_disambiguation|Template:Hospital_disambiguation|Template:Human_name_disambiguation|Template:Human_name_disambiguation_cleanup|Template:Letter_disambiguation|Template:Letter-Number_Combination_Disambiguation|Template:Mathematical_disambiguation|Template:Mil-unit-dis|Template:Number_disambiguation|Template:Phonetics_disambiguation|Template:Place_name_disambiguation|Template:Road_disambiguation|Template:School_disambiguation|Template:Species_Latin_name_abbreviation_disambiguation|Template:Species_Latin_name_disambiguation|Template:Synagogue_disambiguation|Template:Taxonomic_authority_disambiguation|Template:Taxonomy_disambiguation|Template:Wikipedia_disambiguation|Template:Set_index_article|Template:Animal_common_name|Template:Chemistry_index|Template:Enzyme_index|Template:Fungus_common_name|Template:Given_name|Template:Lake_index|Template:Locomotive_index|Template:Molecular_formula_index|Template:Mountain_index|Template:Nickname|Template:Plant_common_name|Template:River_index|Template:Road_index|Template:Ship_index|Template:Sport_index|Template:Storm_index|Template:Surname&titles=' + selection
   
+  url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=1&indexpageids=&prop=extracts|templates&exintro=&explaintext=&tltemplates=Template:Disambiguation|Template:Dmbox|Template:Biology_disambiguation|Template:Call_sign_disambiguation|Template:Caselaw_disambiguation|Template:Chinese_title_disambiguation|Template:Disambiguation_cleanup|Template:Genus_disambiguation|Template:Hospital_disambiguation|Template:Human_name_disambiguation|Template:Human_name_disambiguation_cleanup|Template:Letter_disambiguation|Template:Letter-Number_Combination_Disambiguation|Template:Mathematical_disambiguation|Template:Mil-unit-dis|Template:Number_disambiguation|Template:Phonetics_disambiguation|Template:Place_name_disambiguation|Template:Road_disambiguation|Template:School_disambiguation|Template:Species_Latin_name_abbreviation_disambiguation|Template:Species_Latin_name_disambiguation|Template:Synagogue_disambiguation|Template:Taxonomic_authority_disambiguation|Template:Taxonomy_disambiguation|Template:Wikipedia_disambiguation|Template:Set_index_article|Template:Animal_common_name|Template:Chemistry_index|Template:Enzyme_index|Template:Fungus_common_name|Template:Given_name|Template:Lake_index|Template:Locomotive_index|Template:Molecular_formula_index|Template:Mountain_index|Template:Nickname|Template:Plant_common_name|Template:River_index|Template:Road_index|Template:Ship_index|Template:Sport_index|Template:Storm_index|Template:Surname&titles=' + selection  
   urlString = fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
       pageid = data.query.pageids[0]
       if (pageid == '-1') {
-        noArticle = document.createElement('div')
-        noArticle.setAttribute('id', 'noarticle')
-        noArticle.textContent = "Couldn't find a page for that!"
-        body = document.body
-        body.appendChild(noArticle)
+
+        // If there is no page, get full text search results
+        fullSearch(selection, 0)
       } else if (data['query']['pages'][pageid]['templates']) {
+
+        // Display disambiguation pages
         disambiguate(selection)
       } else {
+
+        // Display extract
         articleTitle = document.createElement('div')
         articleTitle.setAttribute('id', 'title')
         articleTitle.textContent = selection        
@@ -227,10 +346,13 @@ function wikipediaBody(selection){
         body.appendChild(articleBody)
         body.appendChild(separator2)
         body.appendChild(referenceContainer)
-        // Ugly hack to focus sidebar
+        
+        // Focus cursor on sidebar
         if (document.getElementById('focused')) {
           document.getElementById('focused').remove()
-        }        
+        }
+
+        // Get image
         wikipediaImage(selection)
       }
     })
